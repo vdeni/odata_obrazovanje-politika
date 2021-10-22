edc8df7c3dffd36c8ff391b54da00ae1ad9895c6
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-10-22 13:16:00 +0200

    dodao kodove za zupanije, zup i grop_id

--- scrape/01_donacije_lokalni-izbori-2021.py
+++ scrape/01_donacije_lokalni-izbori-2021.py
@@ -30,16 +30,9 @@ kodovi_zupanije = zip(_,
 
 kodovi_zupanije = dict(list(kodovi_zupanije))
 
-with open(os.path.join(data_path,
-                       'zupanije_kodovi.json'),
-          'w+',
-          encoding='utf-8') as outfile:
-    json.dump(kodovi_zupanije,
-              outfile)
-
 # scrape
-for zup in elem_zupanije[0:3]:
-    print(f'\n======>> Prikupljam podatke za: {zup.text}\n')
+for zup_id, zup in enumerate(elem_zupanije[0:2]):
+    print(f'\n===>> {zup_id} / 21 _ Prikupljam podatke za: {zup.text}\n')
 
     zup.click()
 
@@ -50,8 +43,8 @@ for zup in elem_zupanije[0:3]:
         find_elements(by='xpath',
                       value='//select[@ng-model="grop"]/option[@label]')
 
-    for grop in elem_grop:
-        print(f'\n@@@@@@@@@@>> Prikupljam podatke za: {grop.text}')
+    for grop_id, grop in enumerate(elem_grop):
+        print(f'\n======>> Prikupljam podatke za: {grop.text}')
 
         grop.click()
 
@@ -64,7 +57,7 @@ for zup in elem_zupanije[0:3]:
                                    option[@label]''')
 
         for izbori in elem_izbori:
-            print(f'\t======>> Prikupljam podatke za: {izbori.text}')
+            print(f'\t>>> Prikupljam podatke za: {izbori.text}')
 
             izbori.click()
 
@@ -87,8 +80,8 @@ for zup in elem_zupanije[0:3]:
                 donacije_json = driver.\
                     find_element(by='xpath',
                                  value='''//div[contains(text(),
-                                       "IZ-D-IP")]/a[contains(text(),
-                                       "json")]''').\
+                                          "IZ-D-IP")]/a[contains(text(),
+                                          "json")]''').\
                     get_attribute('href')
 
-                donacije_json = requests.get(donacije_json).text
+                donacije_json = requests.get(donacije_json).json()
