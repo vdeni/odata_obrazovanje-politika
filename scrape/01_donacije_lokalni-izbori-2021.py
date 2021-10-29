a7f13680678e67b6383f4306dcb632ac6e8d91b9
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-10-29 15:23:26 +0200

    dodao preskakanje suvisnih zupanijskih izbora

--- scrape/01_donacije_lokalni-izbori-2021.py
+++ scrape/01_donacije_lokalni-izbori-2021.py
@@ -11,6 +11,9 @@ from selenium import webdriver
 data_path = os.path.join('data',
                          '2021-lokalni-izbori_donacije')
 
+# default sleep
+def_sleep = 1.5
+
 # URL aplikacije za dohvacanje financijskih izvjestaja
 base_url = 'https://www.izbori.hr/lokalni2021/financ/1/'
 
@@ -23,13 +26,13 @@ elem_zupanije = driver.\
     find_elements(by='xpath',
                   value='//select[@ng-model="zupanija"]/option[@label]')
 
-# izvuci imena zupanija i napravi dict s kodovima
-_ = [zup.text for zup in elem_zupanije]
+# ucitaj kodove zupanija
+with open(os.path.join(data_path,
+                       'zupanije_kodovi.json'),
+          'r') as infile:
+    kodovi_zupanije = json.load(infile)
 
-kodovi_zupanije = zip(_,
-                      [f'{x:02}' for x in range(1, len(_) + 1)])
-
-kodovi_zupanije = dict(list(kodovi_zupanije))
+counter_zupanije = dict()
 
 # >>>>> scrape
 for zup_id, zup in enumerate(elem_zupanije[0:2]):
@@ -37,7 +40,7 @@ for zup_id, zup in enumerate(elem_zupanije[0:2]):
 
     zup.click()
 
-    time.sleep(2)
+    time.sleep(def_sleep)
 
     # odabir gradova i opcina
     elem_grop = driver.\
@@ -49,40 +52,50 @@ for zup_id, zup in enumerate(elem_zupanije[0:2]):
 
         grop.click()
 
-        time.sleep(2)
+        time.sleep(def_sleep)
 
         # odabir vrste izbora
         elem_izbori = driver.\
             find_elements(by='xpath',
-                          value='''//select[@ng-model="vrstaIzbora"]/
-                                   option[@label]''')
+                          value='//select[@ng-model="vrstaIzbora"]/\
+                                 option[@label]')
 
         for izbori in elem_izbori:
             print(f'\t>>> Prikupljam podatke za: {izbori.text}')
 
+            if izbori.text.startswith('ŽUPAN'):
+                if zup.text in counter_zupanije and\
+                    izbori.text in counter_zupanije[zup.text]:
+                    break
+                elif zup.text in counter_zupanije and\
+                    izbori.text not in counter_zupanije[zup.text]:
+                    counter_zupanije[zup.text] += [izbori.text]
+                elif zup.text not in counter_zupanije:
+                    counter_zupanije[zup.text] = [izbori.text]
+
             izbori.click()
 
-            time.sleep(2)
+            time.sleep(def_sleep)
 
             # odabir obveznika
             elem_obveznici = driver.\
                 find_elements(by='xpath',
-                              value='''//select[@ng-model="obveznik"]/
-                                       option[@label]''')
+                              value='//select[@ng-model="obveznik"]/\
+                                     option[@label]')
 
             for obveznik in elem_obveznici:
                 print(f'\t\t>>> Prikupljam podatke za: {obveznik.text}')
 
                 obveznik.click()
 
-                time.sleep(2)
+                time.sleep(def_sleep)
 
                 # nadji JSON
                 donacije_json = driver.\
                     find_element(by='xpath',
-                                 value='''//div[contains(text(),
-                                          "IZ-D-IP")]/a[contains(text(),
-                                          "json")]''').\
+                                 value='//div[contains(text(),\
+                                        "IZ-D-IP")]/a[contains(text(),\
+                                        "json")]').\
                     get_attribute('href')
 
                 donacije_json = requests.get(donacije_json).json()
