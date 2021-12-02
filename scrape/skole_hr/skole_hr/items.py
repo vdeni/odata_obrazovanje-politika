6761054e375b4a96e7cfed0b71c64510c0356ed5
Denis Vlašiček <denis.vlasicek@gmail.com>
2021-12-02 16:20:55 +0100

    unlist singletone

--- scrape/skole_hr/skole_hr/items.py
+++ scrape/skole_hr/skole_hr/items.py
@@ -1,9 +1,16 @@
 import scrapy
 
 
+def unlist_singleton(singleton_list):
+    """
+    Helper za prebacivanje singleton liste u sam element.
+    """
+    return singleton_list[0]
+
+
 class SkoleHrItem(scrapy.Item):
-    skola_url = scrapy.Field()
-    skola_naziv = scrapy.Field()
-    skola_zupanija = scrapy.Field()
-    skola_mjesto = scrapy.Field()
+    skola_url = scrapy.Field(output_processor=unlist_singleton)
+    skola_naziv = scrapy.Field(output_processor=unlist_singleton)
+    skola_zupanija = scrapy.Field(output_processor=unlist_singleton)
+    skola_mjesto = scrapy.Field(output_processor=unlist_singleton)
     tekst = scrapy.Field()
